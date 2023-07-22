f2330a8e4c4511189963f33fb54defb7d30d0e17
Hugo Montenegro <h@hugomontenegro.com>
2023-07-22 22:15:56 +0200

    multideploy script and getting testnet tokens

---
+++ multideploy.sh
@@ -0,0 +1,16 @@
+# Run the following commands:
+# load .env
+source .env
+
+# deploy on goerli
+forge script script/PeanutV4.s.sol:DeployScript --rpc-url goerli --broadcast --verify -vvvv
+# 0x0e16cd9de31dc2fbd58f1cff09b6e380c4368f95
+
+# deploy on mainnet
+
+# deploy on Gnosis Chain
+
+# Deploy on Starknet
+
+# deploy on Mantle
+
---
+++ script/PeanutV4.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+// deploy Peanut Contract
+// forge script script/PeanutV4.s.sol:DeployScript --fork-url http://localhost:8545 --broadcast
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "../src/PeanutV4.sol";
+
+contract DeployScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Create new peanut contract (with broadcast enabled this will send the tx to mempool)
+        PeanutV4 peanutV4 = new PeanutV4();
+
+        vm.stopBroadcast();
+    }
+}
