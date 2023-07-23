229fff1ee1f66cd4c4e18944e29483bd05e354a2
Hugo Montenegro <h@hugomontenegro.com>
2023-07-23 07:21:57 +0200

    finished all chain deployments

--- multideploy.sh
+++ multideploy.sh
@@ -2,18 +2,21 @@
 # load .env
 source .env
 
+# deploy on mainnet
+
 # deploy on goerli
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url goerli --broadcast --verify -vvvv
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url goerli --etherscan-api-key goerli --broadcast --verify -vvvv
 # 0x0e16cd9de31dc2fbd58f1cff09b6e380c4368f95
+# https://goerli.etherscan.io/address/0x0e16cd9de31dc2fbd58f1cff09b6e380c4368f95#code
 
-# deploy on mainnet
 
 # deploy on Gnosis Chain
 
 # deploy on gnosis chain testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url gnosis-testnet --broadcast -vvvv 
-
+# 0xABCf92BD1c01f2eFe63FfcEaa73F19ec72F0Eba5
+# https://gnosis-chiado.blockscout.com/address/0xABCf92BD1c01f2eFe63FfcEaa73F19ec72F0Eba5
 
 # Deploy on Starknet
 
@@ -39,13 +42,16 @@ forge script script/PeanutV4.s.sol:DeployScript --rpc-url celo-alfajores --broad
 # deploy on Neon EVM
 
 # deploy on Neon EVM testnet
-forge script script/PeanutV4.s.sol:DeployScript --rpc-url neon-devnet --broadcast -vvvv --legacy
+forge script script/PeanutV4.s.sol:DeployScript --rpc-url neon-devnet --broadcast -vvvv --legacy -g 8000
+# 0x8d1a17A3A4504aEB17515645BA8098f1D75237f7
+# https://devnet.neonscan.org/address/0x8d1a17A3A4504aEB17515645BA8098f1D75237f7
 
 # deploy on ZetaChain
 
 # deploy on ZetaChain testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url zetachain-testnet --broadcast -vvvv
 # 0xABCf92BD1c01f2eFe63FfcEaa73F19ec72F0Eba5
+# https://athens3.explorer.zetachain.com/address/0xABCf92BD1c01f2eFe63FfcEaa73F19ec72F0Eba5
 
 # deploy on polygon zkEVM
 
@@ -53,8 +59,12 @@ forge script script/PeanutV4.s.sol:DeployScript --rpc-url zetachain-testnet --br
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url polygon-zkevm-testnet --etherscan-api-key polygon-zkevm-testnet --broadcast --verify -vvvv
 # https://testnet-zkevm.polygonscan.com/address/0x60d5db92eca3ee10ccf60517f910d8154ff62231
 
+
+# deploy on Filecoin FVM
 # deploy on filecoin testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url filecoin-testnet --broadcast -vvvv
+# 0x1851359AB8B002217cf4D108d7F027B63563754C
+# https://calibration.filfox.info/en/address/0x1851359AB8B002217cf4D108d7F027B63563754C
 
 
 
