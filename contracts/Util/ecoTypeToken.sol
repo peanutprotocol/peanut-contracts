a38dfb350167772c04355f7b956378511e9a7891
Albert Brown <albertbrown@Bobingy.local>
2023-08-01 12:05:37 -0700

    test

---
+++ contracts/Util/ecoTypeToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@helix-foundation/currency/contracts/currency/ECO.sol";
+
+contract PeanutECO is ECO {
+    constructor(Policy a, address b, uint256 c, address d) ECO(a, b, c, d) {}
+
+    function freeMint(uint256 amount) public {
+        _mint(msg.sender, amount);
+    }
+}
