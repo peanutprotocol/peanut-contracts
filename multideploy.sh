f754eaa237d1c27014a072e051b593dc07b89e72
Hugo Montenegro <h@hugomontenegro.com>
2023-08-03 17:35:55 +0200

    deployments fix & milkomeda

--- multideploy.sh
+++ multideploy.sh
@@ -1,97 +1,111 @@
-# Run the following commands:
-# load .env
 source .env
 
-# deploy on mainnet
+### mainnet
 
-# deploy on goerli
+# mainnet-goerli
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url goerli --broadcast --verify -vvvv
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url goerli --etherscan-api-key goerli --broadcast --verify -vvvv
 # 0x0e16cd9de31dc2fbd58f1cff09b6e380c4368f95
 # https://goerli.etherscan.io/address/0x0e16cd9de31dc2fbd58f1cff09b6e380c4368f95#code
 
-# deploy on polygon
+
+### polygon
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url polygon --etherscan-api-key polygon --broadcast --verify -vvvv --legacy
 
-# deploy on polygon mumbai
+# polygon mumbai
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url polygon-mumbai --etherscan-api-key polygon --broadcast --verify -vvvv --legacy
 
 
-# deploy on Optimism
+### Optimism
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url optimism --etherscan-api-key optimism --broadcast --verify -vvvv
-# deploy on Optimism testnet
+# Optimism testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url optimism-goerli --etherscan-api-key optimism --broadcast --verify -vvvv
 
 
-# deploy on Gnosis Chain
+### Gnosis Chain
 
-# deploy on gnosis chain testnet
+# gnosis chain testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url gnosis-testnet --broadcast -vvvv 
 # 0xABCf92BD1c01f2eFe63FfcEaa73F19ec72F0Eba5
 # https://gnosis-chiado.blockscout.com/address/0xABCf92BD1c01f2eFe63FfcEaa73F19ec72F0Eba5
 
-# Deploy on Starknet
 
-# deploy on Starknet testnet
-# not evm (not gonna write cairo)
+### Mantle
 
-# deploy on Mantle
-
-# deploy on Mantle testnet
+# Mantle testnet
 # doesn't support eip-1559, have to add --legacy
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url mantle-testnet --broadcast --verify -vvvv --legacy
 # 0x7B36e10AA3ff44576efF4b1AfB80587B9b3BA3a5
 # https://explorer.testnet.mantle.xyz/address/0x7B36e10AA3ff44576efF4b1AfB80587B9b3BA3a5
 
-# deploy on Celo
 
-# deploy on Celo testnet
+### Celo
+
+# Celo testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url celo-alfajores --broadcast -vvvv
 # 0x8d1a17A3A4504aEB17515645BA8098f1D75237f7
 # https://explorer.celo.org/alfajores/address/0x8d1a17A3A4504aEB17515645BA8098f1D75237f7
 
 
-# deploy on Neon EVM
+### Neon EVM
 
-# deploy on Neon EVM testnet
+# Neon EVM testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url neon-devnet --broadcast -vvvv --legacy -g 8000
 # 0x8d1a17A3A4504aEB17515645BA8098f1D75237f7
 # https://devnet.neonscan.org/address/0x8d1a17A3A4504aEB17515645BA8098f1D75237f7
 
-# deploy on ZetaChain
 
-# deploy on ZetaChain testnet
+### ZetaChain
+
+# ZetaChain testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url zetachain-testnet --broadcast -vvvv
 # 0xABCf92BD1c01f2eFe63FfcEaa73F19ec72F0Eba5
 # https://athens3.explorer.zetachain.com/address/0xABCf92BD1c01f2eFe63FfcEaa73F19ec72F0Eba5
 
-# deploy on polygon zkEVM
 
-# deploy on polygon zkEVM testnet
+### polygon zkEVM
+
+# polygon zkEVM testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url polygon-zkevm-testnet --etherscan-api-key polygon-zkevm-testnet --broadcast --verify -vvvv
 # 0x60d5db92eca3ee10ccf60517f910d8154ff62231
 # https://testnet-zkevm.polygonscan.com/address/0x60d5db92eca3ee10ccf60517f910d8154ff62231
 
 
-# deploy on Filecoin FVM
-# deploy on filecoin testnet
+### Filecoin FVM
+# filecoin testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url filecoin-testnet --broadcast -vvvv
 # 0x1851359AB8B002217cf4D108d7F027B63563754C
 # https://calibration.filfox.info/en/address/0x1851359AB8B002217cf4D108d7F027B63563754C
 
 
-# deploy on zkSync
+### zkSync
 
-# deploy on zkSync testnet
+# zkSync testnet
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url zksync-testnet --broadcast -vvvv
 # got stuck on https://github.com/omurovec/foundry-zksync-era
 # https://goerli.explorer.zksync.io/
 # contract address will be at 0x8d1a17A3A4504aEB17515645BA8098f1D75237f7 once fixed
 
-# deploy on Base
+
+### Base
 forge script script/PeanutV4.s.sol:DeployScript --rpc-url base --etherscan-api-key base --broadcast --verify -vvvv
-# v3 
-forge script script/PeanutV3.s.sol:DeployScript --rpc-url base --etherscan-api-key base --broadcast --verify -vv
+
+### Base testnet
+forge script script/PeanutV4.s.sol:DeployScript --rpc-url base-testnet --etherscan-api-key base-testnet --broadcast --verify -vvvv
+forge script script/PeanutV4.s.sol:DeployScript --rpc-url base --etherscan-api-key base --broadcast --verify -vvvv
+
+
+### Milkomeda C1
+forge script script/PeanutV4.s.sol:DeployScript --rpc-url milkomeda-c1 --broadcast --verify -vvvv --legacy
+
+
+# Milkomeda C1 Testnet
+forge script script/PeanutV4.s.sol:DeployScript --rpc-url milkomeda-c1-testnet --broadcast --verify -vvvv --legacy
+
+
+
+############################################
+
 # verify existing
 forge verify-contract \
     --chain-id 8453 \
@@ -104,16 +118,10 @@ forge verify-contract \
 
 forge verify-contract 0x1851359ab8b002217cf4d108d7f027b63563754c --rpc-url base --etherscan-api-key base --verify -vvvv
 
-# deploy on Base testnet
-forge script script/PeanutV4.s.sol:DeployScript --rpc-url base-testnet --etherscan-api-key base-testnet --broadcast --verify -vvvv
-forge script script/PeanutV4.s.sol:DeployScript --rpc-url base --etherscan-api-key base --broadcast --verify -vvvv
+############################################
+### Non EVMS
 
+### Starknet
 
-
-### OPTIONAL ### 
-
-# deploy on Arbitrum
-
-# deploy on Arbitrum testnet
-
-
+# Starknet testnet
+# not evm (not gonna write cairo)
