4997dc27b92e8d4432df26e9d466dda0592c48a2
Alexey Nebolsin <alexey@nebols.in>
2024-01-29 13:58:48 +0400

    Create batchMakeDepositRaffle

--- src/V4/PeanutBatcherV4.sol
+++ src/V4/PeanutBatcherV4.sol
@@ -5,21 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-
-interface IPeanut {
-    function makeDeposit(
-        address _tokenAddress,
-        uint8 _contractType,
-        uint256 _amount,
-        uint256 _tokenId,
-        address _pubKey20
-    ) external payable returns (uint256);
-}
+import "./PeanutV4.2.sol";
 
 contract PeanutBatcherV4 {
     using SafeERC20 for IERC20;
 
-    IPeanut public peanut;
+    PeanutV4 public peanut;
 
     function _setAllowanceIfZero(address tokenAddress, address spender) internal {
         uint256 currentAllowance = IERC20(tokenAddress).allowance(address(this), spender);
@@ -36,7 +27,7 @@ contract PeanutBatcherV4 {
         uint256 _tokenId,
         address[] calldata _pubKeys20
     ) external payable returns (uint256[] memory) {
-        peanut = IPeanut(_peanutAddress);
+        peanut = PeanutV4(_peanutAddress);
         uint256 totalAmount = _amount * _pubKeys20.length;
         uint256 etherAmount;
 
@@ -75,7 +66,7 @@ contract PeanutBatcherV4 {
         uint256 _tokenId,
         address[] calldata _pubKeys20
     ) external payable {
-        peanut = IPeanut(_peanutAddress);
+        peanut = PeanutV4(_peanutAddress);
 
         for (uint256 i = 0; i < _pubKeys20.length; i++) {
             peanut.makeDeposit{value: msg.value}(_tokenAddress, _contractType, _amount, _tokenId, _pubKeys20[i]);
@@ -96,7 +87,7 @@ contract PeanutBatcherV4 {
                 && _amounts.length == _pubKeys20.length && _tokenIds.length == _pubKeys20.length,
             "PARAMETERS LENGTH MISMATCH"
         );
-        peanut = IPeanut(_peanutAddress);
+        peanut = PeanutV4(_peanutAddress);
 
         uint256[] memory depositIndexes = new uint256[](_amounts.length);
 
@@ -125,91 +116,43 @@ contract PeanutBatcherV4 {
 
         return depositIndexes;
     }
+
+    function batchMakeDepositRaffle(
+        address _peanutAddress,
+        address _tokenAddress,
+        uint8 _contractType,
+        uint256[] calldata _amounts,
+        address _pubKey20
+    ) external payable returns (uint256[] memory) {
+        require(
+            _contractType == 0 || _contractType == 1,
+            "ONLY ETH AND ERC20 RAFFLES ARE SUPPORTED"
+        );
+
+        peanut = PeanutV4(_peanutAddress);
+        if (_contractType == 1) {
+            _setAllowanceIfZero(_tokenAddress, _peanutAddress);
+            uint256 totalAmount;
+            for(uint256 i = 0; i < _amounts.length; i++) {
+                totalAmount += _amounts[i];
+            }
+            IERC20(_tokenAddress).safeTransferFrom(msg.sender, address(this), totalAmount);
+        }
+
+        uint256[] memory depositIndexes = new uint256[](_amounts.length);
+
+        for (uint256 i = 0; i < _amounts.length; i++) {
+            uint256 etherAmount;
+
+            if (_contractType == 0) {
+                etherAmount = _amounts[i];
+            }
+
+            depositIndexes[i] = peanut.makeSelflessDeposit{value: etherAmount}(
+                _tokenAddress, _contractType, _amounts[i], 0, _pubKey20, msg.sender
+            );
+        }
+
         return depositIndexes;
     }
 }
-
-///////////////////////////////////////////////////////////////////////////////////////////
-// Unnecessary for now
-///////////////////////////////////////////////////////////////////////////////////////////
-// function batchMakeDepositEther(address _peanutAddress, uint256[] calldata _amounts, address[] calldata _pubKeys20)
-//     external
-//     payable
-//     returns (uint256[] memory)
-// {
-//     require(_amounts.length == _pubKeys20.length, "PARAMETERS LENGTH MISMATCH");
-
-//     peanut = IPeanut(_peanutAddress);
-
-//     uint256[] memory depositIndexes = new uint256[](_amounts.length);
-//     uint256 totalAmount = 0;
-
-//     for (uint256 i = 0; i < _amounts.length; i++) {
-//         totalAmount += _amounts[i];
-
-//         depositIndexes[i] = peanut.makeDeposit(address(0), 0, _amounts[i], 0, _pubKeys20[i]);
-//     }
-
-//     require(msg.value == totalAmount, "INVALID TOTAL ETHER SENT");
-
-//     return depositIndexes;
-// }
-
-// function batchMakeDepositERC20(
-//     address _peanutAddress,
-//     address _tokenAddress,
-//     uint256[] calldata _amounts,
-//     address[] calldata _pubKeys20
-// ) external returns (uint256[] memory) {
-//     require(_amounts.length == _pubKeys20.length, "PARAMETERS LENGTH MISMATCH");
-//     peanut = IPeanut(_peanutAddress);
-
-//     uint256[] memory depositIndexes = new uint256[](_amounts.length);
-
-//     for (uint256 i = 0; i < _amounts.length; i++) {
-//         depositIndexes[i] = peanut.makeDeposit(_tokenAddress, 1, _amounts[i], 0, _pubKeys20[i]);
-//     }
-
-//     return depositIndexes;
-// }
-
-// function batchMakeDepositERC721(
-//     address _peanutAddress,
-//     address _tokenAddress,
-//     uint256[] calldata _tokenIds,
-//     address[] calldata _pubKeys20
-// ) external returns (uint256[] memory) {
-//     require(_tokenIds.length == _pubKeys20.length, "PARAMETERS LENGTH MISMATCH");
-
-//     peanut = IPeanut(_peanutAddress);
-
-//     uint256[] memory depositIndexes = new uint256[](_tokenIds.length);
-
-//     for (uint256 i = 0; i < _tokenIds.length; i++) {
-//         depositIndexes[i] = peanut.makeDeposit(_tokenAddress, 2, 0, _tokenIds[i], _pubKeys20[i]);
-//     }
-
-//     return depositIndexes;
-// }
-
-// function batchMakeDepositERC1155(
-//     address _peanutAddress,
-//     address _tokenAddress,
-//     uint256[] calldata _amounts,
-//     uint256[] calldata _tokenIds,
-//     address[] calldata _pubKeys20
-// ) external returns (uint256[] memory) {
-//     require(
-//         _amounts.length == _pubKeys20.length && _tokenIds.length == _pubKeys20.length, "PARAMETERS LENGTH MISMATCH"
-//     );
-//     peanut = IPeanut(_peanutAddress);
-
-//     uint256[] memory depositIndexes = new uint256[](_amounts.length);
-
-//     for (uint256 i = 0; i < _amounts.length; i++) {
-//         depositIndexes[i] = peanut.makeDeposit(_tokenAddress, 3, _amounts[i], _tokenIds[i], _pubKeys20[i]);
-//     }
-
-//     return depositIndexes;
-// }
-///////////////////////////////////////////////////////////////////////////////////////////
